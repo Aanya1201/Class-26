dc2c7e323bee0e3108b8d4b0bbb6650fc14aa82e
Aanya <aanyaajmera@gmail.com>
2020-11-24 17:48:39 +0530

    Message

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
 var backgroundImg,platform;
 
 function preload() {
@@ -20,7 +19,7 @@ function setup(){
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
+    box2 = new Box(920,320,70,70)
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
